fd5934bd0493d84ad316e7685014f49115c43654
alfathmuqoddas <alfath.muqoddas@gmail.com>
2021-12-27 13:31:04 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React App Simplified
+## smaller create-react-app
+git clone it, `npm install` and start by `npm start`
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+		<h1>If you see this your code works</h1>
+    </div>
+  );
+}
+
+export default App;
