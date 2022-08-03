2b16d99f678152f434b5b9cece31b162909ff225
Alfath Muqoddas <85182650+alfathmuqoddas@users.noreply.github.com>
2022-08-03 12:52:57 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,29 @@
-import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   let [count, setCount] = useState(0);
   const increment = () => {
-	counter = count + 1
-	setCount(counter) 
-  }
-  
+    setCount((count += 1));
+  };
+
   const decrement = () => {
-	counter = count - 1
-	setCount(counter) 
-  }
-  
+    setCount((count -= 1));
+  };
+
   const reset = () => {
-	setCount(0) 
-  }
-  
+    setCount(0);
+  };
+
   return (
     <div className="App">
-		<h1>If you see this your code works like a charm</h1>
-		<h2>Begin editing</h2>
-	  <h1>{count}</h1>
-          <button onClick={increment}>+</button>
-          <button onClick={decrement}>-</button>
-          <button onClick={reset}>Reset</button>
+      <h1>If you see this your code works like a charm</h1>
+      <h2>Begin editing</h2>
+      <h1>{count}</h1>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
 
-export default App
+export default App;
