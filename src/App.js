41f1a8e9f866157607026b87e9f65cb83c5455f6
Alfath Muqoddas <85182650+alfathmuqoddas@users.noreply.github.com>
2022-08-03 12:45:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import {useState, useEffect} from 'React';
 
 function App() {
+  let [count, setCount] = useState(0);
+  const increment = () => {
+	counter = count + 1
+	setCount(counter) 
+  }
+  
+  const decrement = () => {
+	counter = count - 1
+	setCount(counter) 
+  }
+  
+  const reset = () => {
+	setCount(0) 
+  }
+  
   return (
     <div className="App">
 		<h1>If you see this your code works like a charm</h1>
 		<h2>Begin editing</h2>
+	  <h1>{count}</h1>
+          <button onClick={increment}>+</button>
+          <button onClick={decrement}>-</button>
+          <button onClick={reset}>Reset</button>
     </div>
   );
 }
